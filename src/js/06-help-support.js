1167499b2e49594e72aff056730c1d94ebe2205c
Enrico <enrico@bacosoft.com>
2024-04-16 17:50:40 +0200

    Update 06-help-support.js

--- src/js/06-help-support.js
+++ src/js/06-help-support.js
@@ -1,16 +1,16 @@
 // Página dentro de un iframe
 if (window !== window.parent) {
-    document.documentElement.classList.add('help-window');
-    
-    // Cambios en la historia de navegación
-    window.addEventListener("load", function(event) {
-        window.parent.postMessage({'message': 'pageLoaded', 'currentPage': window.location.href}, '*');
-    });
+  document.documentElement.classList.add('help-window');
 
-    // escuchamos peticiones de navegar atrás
-    window.addEventListener('message', function(event) {
-        if (event.data === 'historyBack') {
-            window.history.back();
-        }
-    });
+  // Cambios en la historia de navegación
+  window.addEventListener('load', function(event) {
+    window.parent.postMessage({ 'message': 'pageLoaded', 'currentPage': window.location.href }, '*');
+  });
+
+  // escuchamos peticiones de navegar atrás
+  window.addEventListener('message', function(event) {
+    if (event.data === 'historyBack') {
+      window.history.back();
+    }
+  });
 }
