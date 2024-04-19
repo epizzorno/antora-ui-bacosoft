9ae8a2b7b3ca4dd59bc90717eb7be03c09b31256
Enrico <enrico@bacosoft.com>
2024-04-19 18:25:41 +0200

    Ponemos el help-window en un script del head del html
    Es la única manera que se ejecute cuando toca

--- src/js/06-help-support.js
+++ src/js/06-help-support.js
@@ -1,7 +1,5 @@
 // Página dentro de un iframe
 if (window !== window.parent) {
-  document.documentElement.classList.add('help-window')
-
   // Cambios en la historia de navegación
   window.addEventListener('load', function (event) {
     window.parent.postMessage({ message: 'pageLoaded', currentPage: window.location.href }, '*')
