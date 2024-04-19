76f12e371276498ed951c8efcefc4c3c62a8a440
epizzorno <enrico@bacosoft.com>
2024-04-19 18:45:19 +0200

    Javascript help (#36)
    * Javascript para soportar comunicación con página padre del iframe
    
    * Update 06-help-support.js
    
    * Update 06-help-support.js
    
    * Ponemos el help-window en un script del head del html
    
    Es la única manera que se ejecute cuando toca

---
+++ src/js/06-help-support.js
@@ -0,0 +1,14 @@
+// Página dentro de un iframe
+if (window !== window.parent) {
+  // Cambios en la historia de navegación
+  window.addEventListener('load', function (event) {
+    window.parent.postMessage({ message: 'pageLoaded', currentPage: window.location.href }, '*')
+  })
+
+  // escuchamos peticiones de navegar atrás
+  window.addEventListener('message', function (event) {
+    if (event.data === 'historyBack') {
+      window.history.back()
+    }
+  })
+}
